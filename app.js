36a4b9be2766db6c45c28e88c33325e916c2ea4d
bainiwaj <43765015+bainiwaj@users.noreply.github.com>
2019-11-01 17:05:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // SERVER SIDE CODE
 
 var express = require('express');
-var mysql = require('./dbcon.js');
+//var mysql = require('./dbcon.js');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -16,31 +16,115 @@ app.engine('handlebars', handlebars.engine);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/static', express.static('public')); // where static .js files are served
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
-app.set('mysql', mysql);
+//app.set('port', 3000);
+//app.set('mysql', mysql);
 
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/users.js');
+
+app.use(session({ 
+  secret: 'lol', 
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 100 * 60 * 60 * 24 * 30} //30 days
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/**********************************************
+home landing page here
+*********************************************/
 //brings up homepage
 app.get('/', function(req, res){
-	res.render('home.handlebars')
+	res.render('home.handlebars');
 });
 
 // OTHER ROUTES HERE...
 
-
-//bad url route
-app.use(function(req, res){
-	res.status(404);
-	res.render('404');
+//login route
+app.get('/login', function(req, res){
+	res.render('login.handlebars');
 });
 
-//bad syntax route
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.type('plain/text');
-	res.status(500);
-	res.render('500');
+app.get('/woo', loggedIn, function(req, res){
+	res.send("logged in");
 });
 
-app.listen(app.get('port'), function(){
+/*************************************************************************
+Passport strats
+*************************************************************************/
+//passport documentation on configuring strategy http://www.passportjs.org/packages/passport-local/
+passport.use(new LocalStrategy(
+	{usernameField: 'email',
+	},
+	function(username, password, done){
+		User.findOne({
+			where: {email: username}
+		}).then(function(user){
+			if(!user){
+				//console.log("wrong username");
+				return done(null, false, {message: 'Username is incorrect'});
+			}
+			if (!(user.password == password)){
+				//console.log("wrong password");
+				return done(null, false, {message: 'Password is incorrect'});
+			}
+			if (user.password == password){
+				//console.log("correct");
+				return done(null, user, {message: 'logged in'});
+
+			}
+		}).catch(err => done(err));
+	}));
+
+
+//now to serialize user and deserialize user for sessions
+
+passport.serializeUser(function(user, done){
+	done(null, user.email);
+});
+
+
+passport.deserializeUser(function(user, done){
+	done(null, user);
+});
+
+/****************************************
+middleware that will auth
+************************************/
+app.post('/login', passport.authenticate('local', { 
+	successRedirect: '/woo',
+    failureRedirect: '/login'
+}), function(req, res){
+	//nothing in callback	
+});
+
+//logout 
+app.get('/logout', function(req, res){
+	req.logout();
+	res.redirect('/');
+})
+
+//check if loggedIn 
+function loggedIn(req, res, next){
+	if (req.isAuthenticated()){
+		next();
+	}
+	else{
+		res.redirect('/');
+	}
+}
+/**************************
+End middleware code
+***************************/
+
+app.listen(3000, function(){
+    console.log("server started");
+})
+
+/*app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; Press Ctrl-C to terminate.');
-});
+});*/
